84a9a26adf02f8a1f331b0c26bc5dac10a96b116
Umairus <mumair.bscs19seecs@seecs.edu.pk>
2023-01-24 15:19:10 +0500

    comparison of queried values and values from table

--- demo.clj
+++ demo.clj
@@ -9,121 +9,282 @@
 
 
 
+(def tablemap(atom []))
+;;We can store the data by fetching the index of the tablemap and insert the record map into the list of records
+
+
+;; (def singletable(atom []))
+(def tableData (atom []) )
 
 
 
 
-(defn createRecord[]
+
+
+(defn create-table [tablename data]
+
   
   
-
-(let  [numOfFields 1]
-
-
-  (def record (atom {}))
-
-  ;; (println @record)
-  (dotimes [n numOfFields]
-
-    (println "Enter field and value:")
-
-
-    (let [field (read-line)]
-
-      (let [value (read-line)]
-        
-        
-        (reset! record (assoc @record (keyword field) value))))
-
-    ;; (println @record)
-    ))
+  (reset! tablemap (conj @tablemap tablename data))
+  (reset! tableData (conj @tableData ()))
+  
+  (println @tablemap)
+  
   
   
   )
 
-
-(defn createTable []
-
-  
-  (def Table (atom []))
-  
-  
-
   ;; (println @Table)
+  
 
-  (def numOfRecords 2)
+(defn insert [tablename data]
 
-  (dotimes [n numOfRecords]
-
-
-    (createRecord)
-    (reset! Table (conj @Table @record)))
-
-  ;; (println @Table)
-  )
-
-
-
-
-(defn selectRecords [key]
-
-  ;; (println key)
-  (doseq [n @Table] 
-    ;; (println (class (n :salary)))
+;; (println tablename data)
+  
+  
+  ;;search for table
+  
+  
+    ;; loop over the tablemap elements and store the index of the correct element
+  (dotimes [n (count @tablemap)]
     
-    
-    (if (contains? n :salary)
-       (if  (= 1000  (Integer/parseInt (get n :salary)))
-         (println "Record Found: " n)
-         (println "No Records Found")) 
-      (println "key not found");
+    (if (= tablename (nth @tablemap n))
+      (def tableIndex n)
       )
-   
+    
+    )
+  (println tableIndex)
+  
+  
+  
+  (dotimes [n (count @tablemap)]
+    
+    (if (= tablename (nth @tablemap n))
+      (def types  (vals (nth @tablemap (eval (+ n 1))))) 
+      )
     
     )
   
-)
+  (println types)
 
-
-
-
-
-(defn deleteRecords [key]
   
+  ;; (println "this is types: "types)
   
-  (defn remove-indexed [v n]
-    (into (subvec v 0 n) (subvec v (inc n))))
+  ;; (println @tablemap)
+  (def mapList (into [] @tablemap))
   
-   (doseq [n @Table]
-
-    ;; (println @Table)
-    ;; (println (get n :id))
+  ;; (println mapList)
+  
+  (def recordData (into [] (vals data)))
+  ;; (println recordData)
+  
+  (dotimes [n (count types)]
     
-
-    (println (class (get key 2)))
-    (if (contains? n (keyword (get key 0)))
-      (if (=  (Integer/parseInt (get key 2)) (Integer/parseInt (get n :salary)))
+    ;; (println (nth types n))
+    
+    (if (= (nth types n) :string)
+      
+      (do
+        (println "this data entry shud be a string") 
         
-        (do
-          (reset! Table (remove-indexed @Table (.indexOf @Table n)))
-          (println n " has been employeeted"))
+        (if (string? (nth recordData n)) 
+          (do 
+            (println "the entered type is correct")
+            )
+          (do 
+            (println "the entered type is incorrect") 
+            )
+          
+          
+          
+          
+          )
+        
+        
+        )
+      (do
+        
+        (println "this entry shud be a number")
+        (if (number? (nth recordData n))
+          (println "the entered type is correct")
+          (println "the entered type is incorrect")
+          
+          )
+        
+        
         )
       
       )
     
     
     
+    ;; (print "this is table index" tableIndex)
+    
     )
-         
-         
-         
+  
+  
+  (def dataIndex (eval (/ tableIndex 2)))
+  
+  (println "This is all the tables" @tableData)
+  
+  
+  
+  (swap! tableData update-in [dataIndex] conj data)
+  (println "This is all the tables" @tableData)  
+  
+  
+  )
+(defn select-records [tablename query]
+
+  
+  (dotimes [n (count @tablemap)]
+
+    (if (= tablename (nth @tablemap n))
+      (def tableMapIndex n)))
+  
+  ;; (println tableMapIndex)
+  
+  
+  (def thisTableIndex  (eval (/ tableMapIndex 2)))
+  
+  ;; (println thisTableIndex)
+  
+  
+  (dotimes [n (count (nth @tableData thisTableIndex))]
+    
+    (println (nth (nth @tableData thisTableIndex) n))
+    
+    ;;records of single tables have been acceessed.
+    
+    
+    )
+ 
+  
+  (println "this is the query: "query)
+  
+  (def queryElements (flatten (vals query)))
+  
+  (println queryElements )
+  (println "this is query elements: "(nth queryElements 0))
+  
+  (if (= := (nth queryElements 0)) 
+    
+    
+     (dotimes [n (count (nth @tableData thisTableIndex))]
+       
+       (println "The queried value is: " (nth queryElements 2))
+       (println "The value from table is:"  (get (nth (nth @tableData thisTableIndex) n)  (nth queryElements 1)))
+;; (println "The desired key value from table" (Integer/parseInt (get (nth (nth @tableData thisTableIndex) n)  (nth queryElements 1)))) 
+       
+       (println (compare (Integer/parseInt (get (nth (nth @tableData thisTableIndex) n) (nth queryElements 1))) (Integer/parseInt (nth queryElements 2)) ))
+       
+       (if (contains? (nth (nth @tableData thisTableIndex) n) (nth queryElements 1)) 
+         (if (= (Integer/parseInt (get (nth (nth @tableData thisTableIndex) n) (nth queryElements 1))) (Integer/parseInt (nth queryElements 2))) 
+           (println "Record Found: "(nth (nth @tableData thisTableIndex) n) )
          
          )
+    ;;records of single tables have been acceessed.
+       )
+    )
+     
+  )
+    
+    
+    (if (= :>= (nth queryElements 0))
+
+      (println "Greater than Equal to"))
   
-(createTable)
+    (if (= :<= (nth queryElements 0))
+
+      (println "Less than Equal to"))
+  
+    (if (= :> (nth queryElements 0))
+
+      (println "Greater than"))
+  
+    (if (= :< (nth queryElements 0))
+
+      (println "Less than"))
+  
+  )
+
+
+
+
+
+;; (defn deleteRecords [key]
+  
+  
+;;   (defn remove-indexed [v n]
+;;     (into (subvec v 0 n) (subvec v (inc n))))
+  
+;;    (doseq [n @Table]
+
+;;     ;; (println @Table)
+;;     ;; (println (get n :id))
+    
+
+;;     (println (class (get key 2)))
+;;     (if (contains? n (keyword (get key 0)))
+;;       (if (=  (Integer/parseInt (get key 2)) (Integer/parseInt (get n :salary)))
+        
+;;         (do
+;;           (reset! Table (remove-indexed @Table (.indexOf @Table n)))
+;;           (println n " has been employeeted"))
+;;         )
+      
+;;       )
+    
+    
+    
+;;     )
+         
+         
+         
+         
+;;          )
+
+
+
+
+
+  
+(create-table :employee {:id :number :first-name :string :last-name :string :salary :number})
+
+
+
+(create-table :ducks {:id :number :first-name :string :last-name :string :salary :number :wings :number})
+
+(create-table :applicants {:id :number :first-name :string :last-name :string :salary :number :wings :number})
+
+
+(insert :employee {:id 1 :first-name "Alan" :last-name "Kay" :salary 100500})
+
+(insert :employee {:id 2 :first-name "Alanasdasd" :last-name "Kay" :salary 1004})
+(insert :employee {:id 3 :first-name "Rakesh Cristoval" :last-name "Cheens" :salary 119})
+
+
+
+
+
+(insert :ducks {:id 2 :first-name "HEHEHEHE" :last-name "Rakesh" :salary 123123 :wings 2242})
+(insert :applicants {:id 34534 :first-name "HE" :last-name "sssssd" :salary 123122342343 :wings 2242})
+
+
+
+(select-records :employee {:where [:= :salary 10000]})
+
+
+
+
+
+
 ;; (selectRecords["salary" "equals" 1000])
+;; (println @Table)
+;; (deleteRecords["salary" "equals" "1"])
+;; (println @Table)
 
 
-(println @Table)
-(deleteRecords["salary" "equals" "1"])
-(println @Table)
+;; rn the code works perfectly to create arbitrary records in a table of aribitrary length
+;; now i just need to figure out a way to include type checking by using a global variable
+;; and make each table have its own type checking using one global variable
--- testbed.clj
+++ testbed.clj
@@ -1,1 +1,33 @@
-(if 0 (println "Duck"))
+
+;; (def singletable (atom []))
+;; (def alltables (atom [singletable]))
+
+
+;; (swap! alltables (conj @(nth @alltables 0) {:duck "hehe"} ))
+
+;; (println deref(get @alltables 0))
+
+
+;; (def singletable (atom []))
+;; (def alltables (atom [singletable]))
+
+;; (swap! (nth @alltables 0) conj {:duck "hehe"})
+
+;; (println @(nth @alltables 0))
+
+;; (def alltables (atom [[{:id "hehesdshjsdfs222"}],[{:id "hehesdshjsdfs"}]]))
+
+
+;; (println @alltables)
+
+;; (reset! (nth @alltables 0) [(conj (nth @alltables 0) {:id "hehe"})])
+
+;; (println @alltables)
+
+(def alltables (atom [[{:id "hehesdshjsdfs222"}],[{:id "hehesdshjsdfs"}]]))
+
+(println @alltables)
+
+(swap! alltables update-in [0] conj {:id "hehe"})
+
+(println @alltables)
